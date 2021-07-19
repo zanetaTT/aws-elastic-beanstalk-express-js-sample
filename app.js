587eb0271c1bdf7a1b035e542f29ec7673977444
Zaneta Asare <zaneta.asare@turntabl.io>
2021-07-19 16:11:47 +0000

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Zaneta!'));
+app.get('/', (req, res) => res.send('Hi Zaneta! Complete App'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
